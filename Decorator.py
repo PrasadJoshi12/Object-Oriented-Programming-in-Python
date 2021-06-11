6c324c65f842f6bd0d6715c8cb42bcf1f5ef3107
PrasadJoshi12 <p.joshi86753@gmail.com>
2021-06-11 14:49:06 +0530

    Decorator Example

---
+++ Decorator.py
@@ -0,0 +1,18 @@
+# Decorator Example
+class Error_check:
+  def __init__(self, function):
+    self.function = function 
+  def __call__(self, *args):
+    if any([isinstance(i, str) for i in args]):
+      raise Exception("Please Enter the Numeric Value")
+    else:
+      return self.function(args)
+      
+@Test
+def add_num(args):
+  return sum(args)
+
+print(add_num(1,2,3,4))
+  
+#Output:
+10
